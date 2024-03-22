a9fb7e0eb64c2e821540929e88863eab992569d3
saaaaadc <saadnaushad125@gmail.com>
2024-03-22 15:55:34 +0530

    hhh
    hihi

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -193,40 +193,6 @@ class _homeState extends State<home> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  height: 55,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    color: Colors.grey[200],
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                  padding: EdgeInsets.symmetric(horizontal: 25, vertical: 5),
-                  child: Row(
-                    children: [
-                      Icon(Icons.search_outlined),
-                      SizedBox(width: 10),
-                      Flexible(
-                          flex: 4,
-                          child: TextField(
-                            decoration: InputDecoration(
-                                hintText: "Search...",
-                                border: InputBorder.none),
-                          )),
-                      Container(
-                        height: 25,
-                        width: 1.5,
-                        color: Colors.grey,
-                      ),
-                      IconButton(
-                          onPressed: () {},
-                          icon: Icon(Icons.keyboard_option_key_outlined)),
-                    ],
-                  ),
-                ),
-              ),
-              SizedBox(height: 20),
               Stack(
                 children: [
                   Container(
